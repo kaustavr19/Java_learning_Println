fc51f25593d681e425159add4edaded5f49e1c20
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-11 18:37:02 +0530

    --- Auto-Git Commit ---

---
+++ data_type.java
@@ -0,0 +1,10 @@
+public class data_type {
+    public static void main(String []args){
+        int a;
+        a=10;
+        System.out.println(a);
+
+        double d = 1.1111111;
+        System.out.println(d);
+    }
+}
